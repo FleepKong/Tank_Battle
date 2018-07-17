6e58e3c702e0007ff3a380693145f5a1e4e7be14
DESKTOP-82L260M\Papi Kong <felipepaulomachado90@gmail.com>
2018-07-16 22:59:30 -0400

    added the variables for the clamp

--- Tank_Battle/Source/Tank_Battle/Private/TankTurret.cpp
+++ Tank_Battle/Source/Tank_Battle/Private/TankTurret.cpp
@@ -8,7 +8,7 @@ void UTankTurret::Spin(float RelativeSpeed)
 	RelativeSpeed = FMath::Clamp<float>(RelativeSpeed, -5, 5);
 	auto ElavationChange = RelativeSpeed * MaxDegreesPerSecond * GetWorld()->DeltaTimeSeconds;
 	auto RawNewElavation = RelativeRotation.Yaw + ElavationChange;
-	auto Elavation = FMath::Clamp<float>(RawNewElavation, -360, 360); //gotta use the absolut value to make the turret go all aroundwithout restting
+	auto Elavation = FMath::Clamp<float>(RawNewElavation,  MinRotation, MaxRotation); //gotta use the absolut value to make the turret go all aroundwithout restting
 	SetRelativeRotation(FRotator(0, Elavation, 0));
 }
 
